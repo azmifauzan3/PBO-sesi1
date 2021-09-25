b38d954230567e7e378c3ac65f26a394c1f0ae6b
azmifauzan3 <81280706+azmifauzan3@users.noreply.github.com>
2021-09-25 20:02:10 +0700

    Add files via upload

---
+++ java12.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhammad Azmi Fauzan
+ */
+public class java12 {
+    public static void main (String[]args){
+        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar "+namaTeman+" Sangat mudah   ");
+    }
+}
