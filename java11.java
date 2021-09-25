67846ba31c318efb08c731ac88f62e583cde5b29
azmifauzan3 <81280706+azmifauzan3@users.noreply.github.com>
2021-09-25 20:01:49 +0700

    Add files via upload

---
+++ java11.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhammad Azmi Fauzan
+ */
+public class java11 {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("Dan Sangat Menyenangkan");
+    }
+    
+}
+    
+
